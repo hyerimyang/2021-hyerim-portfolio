5ed31310125aee3e30417aaf6cf8202bb59aba35
hyerimyang <did3296@naver.com>
2021-05-25 22:28:21 +0900

    layout 수정

--- src/components/about/AboutTemplate.js
+++ src/components/about/AboutTemplate.js
@@ -11,7 +11,7 @@ const AboutTemplate = () => {
     <Wrapper>
       <Introduce />
       <Favorite />
-      <Question />
+      {/* <Question /> */}
       <Career />
       <Question2 />
     </Wrapper>
